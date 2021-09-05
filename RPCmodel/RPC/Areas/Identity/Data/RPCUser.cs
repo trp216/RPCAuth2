de361c9f49ee5212bbaa78761b652e64b1010629
Alejandra Diaz <alejandradp19@gmail.com>
2021-09-04 19:20:55 -0500

    First commit. Skeleton of the project.

---
+++ RPCmodel/RPC/Areas/Identity/Data/RPCUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RPC.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the RPCUser class
+    public class RPCUser : IdentityUser
+    {
+    }
+}
